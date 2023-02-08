f309fbff69de2e39cea75b08b1b17afce367d3b7
EuniceleiDelacruz27 <unitlei27@gmail.com>
2023-02-08 16:34:13 +0800

    added files

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var displayedprice = function(){
+    var magicbeans = document.getElementById("magic").value * 12;
+    var metalthing = document.getElementById("metal").value * 3;
+    var totalprice = magicbeans+metalthing;
+    return alert(totalprice);
+}
